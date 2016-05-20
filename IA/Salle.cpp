ecb6ca6aa6ea32315fc611f4cf678ae21e6e71b8
Viet Khang <vietkhangleho@gmail.com>
2016-05-20 16:17:29 +0200

    Implémentation Salle

--- IA/Salle.cpp
+++ IA/Salle.cpp
@@ -1,11 +1,51 @@
 #include "Salle.h"
+#include <cmath>
 
-Salle::Salle()
+using namespace std;
+
+Salle::Salle(int ligne, int colonne, bool stock, int nbEmpLibres, const std::map<Direction, Salle *> lesVoisins) :
+    m_position (ligne, colonne),
+    m_emplacementsLibres (nbEmpLibres, true),
+    m_voisins (lesVoisins),
+    m_stock (stock)
 {
-    //ctor
+
 }
 
 Salle::~Salle()
 {
-    //dtor
+
+}
+
+const std::pair<int, int> & Salle::getPosition() const
+{
+    return m_position;
+}
+
+Salle * Salle::voisin(const Direction & laDirection)
+{
+    if(m_voisins.find(laDirection) != m_voisins.end()) // si il y un voisin a la direction donne
+        return m_voisins[laDirection];
+
+    return nullptr; // sinon on retourne
+}
+
+int Salle::nbEmplacementsLibres() const
+{
+    int nb = 0;
+    for(const bool & b : m_emplacementsLibres)
+        if(b) nb ++;
+
+    return nb;
+}
+
+bool Salle::possedeStock() const
+{
+    return m_stock;
+}
+
+std::pair<int, int> Salle::distanceParRapportA(const Salle & uneSalle) const
+{
+    return make_pair(abs(m_position.first - uneSalle.m_position.first),
+                     abs(m_position.second - uneSalle.m_position.second));
 }
--- IA/Salle.h
+++ IA/Salle.h
@@ -1,13 +1,36 @@
 #ifndef SALLE_H
 #define SALLE_H
 
+#include <utility>
+#include <vector>
+#include <map>
+
+enum Direction
+{
+    Nord,
+    Est,
+    Sud,
+    Ouest,
+
+    NbDeDirections
+};
 
 class Salle
 {
 public:
-    Salle();
+    Salle(int ligne, int colonne, bool stock, int nbEmplacementsLibres, const std::map<Direction, Salle *> voisins);
     virtual ~Salle();
+
+    const std::pair<int, int> & getPosition()  const;
+    Salle *                     voisin(const Direction & laDirection);
+    int                         nbEmplacementsLibres()  const;
+    bool                        possedeStock()  const;
+    std::pair<int, int>         distanceParRapportA(const Salle & uneSalle)  const;
 private:
+    const std::pair<int, int>              m_position;
+    std::vector<bool>                      m_emplacementsLibres;
+    std::map<Direction, Salle *>           m_voisins;
+    const bool                             m_stock;
 };
 
 #endif // SALLE_H
