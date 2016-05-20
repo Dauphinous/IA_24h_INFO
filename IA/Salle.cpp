b0d86c4eb69eacc62a342b734bd03c4e36adcc90
Viet Khang <vietkhangleho@gmail.com>
2016-05-20 17:09:35 +0200

    Salle.h

--- IA/Salle.cpp
+++ IA/Salle.cpp
@@ -3,11 +3,11 @@
 
 using namespace std;
 
-Salle::Salle(int ligne, int colonne, bool stock, int nbEmpLibres, const std::map<Direction, Salle *> lesVoisins) :
+Salle::Salle(int ligne, int colonne, int distanceStock, int nbEmpLibres, const std::map<Direction, Salle *> lesVoisins) :
     m_position (ligne, colonne),
     m_emplacementsLibres (nbEmpLibres, true),
     m_voisins (lesVoisins),
-    m_stock (stock)
+    m_distance (distanceStock)
 {
 
 }
@@ -41,11 +41,16 @@ int Salle::nbEmplacementsLibres() const
 
 bool Salle::possedeStock() const
 {
-    return m_stock;
+    return m_distance == 0;
 }
 
 std::pair<int, int> Salle::distanceParRapportA(const Salle & uneSalle) const
 {
     return make_pair(abs(m_position.first - uneSalle.m_position.first),
                      abs(m_position.second - uneSalle.m_position.second));
+}
+
+int Salle::scorePossible() const
+{
+    return 4 * (m_distance + 1);
 }
