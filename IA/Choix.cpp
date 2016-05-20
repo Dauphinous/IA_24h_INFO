3877ab7700b48b4e333455912cec5dbc85d48ade
Viet Khang <vietkhangleho@gmail.com>
2016-05-20 16:26:34 +0200

    correction erreur

--- IA/Choix.cpp
+++ IA/Choix.cpp
@@ -19,10 +19,10 @@ void Choix::partie()
     int nb_colonne = 0;
     string casiers;
     int nb_ordre;
-    Bouteille couleur_active = Bouteille.ROUGE;
+    Bouteille couleur_active = ROUGE;
     // Finalisation du « create » du protocole du Grand Ordonnateur.
     // - connexion
-    m_tcpGdOrdo->connexion(hote, port);
+    m_tcpGdOrdo->connexion(m_hote, m_port);
     // Réponse au « create » du protocole du Grand Ordonnateur.
     // - envoi du nom de notre équipe
     m_tcpGdOrdo->envoiChaine("CUMULONIMBUS");
@@ -35,27 +35,27 @@ void Choix::partie()
     nb_colonne = m_tcpGdOrdo->receptionEntier();
     // - réception des casiers (nombre d'emplacements et position de
     //   l'escalier) de la cave
-    casiers = tcpGdOrdo->receptionChaine();
+    casiers = m_tcpGdOrdo->receptionChaine();
     // - réception du nombre de manutentionnaires ; 3 ici
-    m_nb_manutentionnaire = tcpGdOrdo->receptionEntier();
+    m_nb_manutentionnaire = m_tcpGdOrdo->receptionEntier();
     // - réception de l'ordre (entre 1 et le nombre de manutentionnaires)
     //   dans lequel l'application singeant notre manutentionnaire joue ; 2
     //   ici
-    nb_ordre = tcpGdOrdo->receptionEntier();
+    nb_ordre = m_tcpGdOrdo->receptionEntier();
 
     if(nb_ordre == 1){
-        m_couleur_nous = Bouteille.ROUGE
+        m_couleur_nous = ROUGE;
     }
     else if(nb_ordre == 2){
-        m_couleur_nous == Bouteille.BLANC;
+        m_couleur_nous = BLANC;
     }
     else
     {
-        m_couleur_nous == Bouteille.ROSE;
+        m_couleur_nous = ROSE;
     }
     // Premier tour de jeu :
     // - réception des actions du premier manutentionnaire
-    tcpGdOrdo->receptionChaine();
+    m_tcpGdOrdo->receptionChaine();
     // - envoi de nos actions (comme deuxième manutentionnaire) ; ici, pour
     //   changer de l'envoi de nos actions du second tour de jeu, envoi des
     //   actions une à une
@@ -65,7 +65,7 @@ void Choix::partie()
 
         }
         else{
-            tcpGdOrdo->receptionChaine();
+            m_tcpGdOrdo->receptionChaine();
         }
     }
 
